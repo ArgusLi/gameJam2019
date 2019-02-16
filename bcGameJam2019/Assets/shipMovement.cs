0efdd8c0698fa7f806f6d18234caa125fa159fd3
Argus Li <argus@thembp.wifi.bcit.ca>
2019-02-16 01:11:00 -0800

    Finally got the ship to scroll

--- bcGameJam2019/Assets/shipMovement.cs
+++ bcGameJam2019/Assets/shipMovement.cs
@@ -7,6 +7,7 @@ public class shipMovement : MonoBehaviour
 {
     public float speed;
     public Camera thiscam;
+    public float shipscrollspeed;
 
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
@@ -15,10 +16,22 @@ public class shipMovement : MonoBehaviour
     private float bottombound;
     private float rightbound;
 
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
         // setting bounds for camera
         float vertextent = thiscam.orthographicSize;
         float horzextent = vertextent;
@@ -26,17 +39,9 @@ public class shipMovement : MonoBehaviour
         rightbound = thiscam.transform.position.x + horzextent;
         topbound = thiscam.transform.position.y + vertextent;
         bottombound = thiscam.transform.position.y - vertextent;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 moveinput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveinput.normalized * speed;
-    }
+        moveVelocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed, Input.GetAxisRaw("Vertical")*speed + shipscrollspeed);
 
-    void FixedUpdate()
-    {
 
         Vector2 newpos = rb.position + moveVelocity * Time.fixedDeltaTime;
         if (newpos.y >= topbound)
@@ -55,7 +60,7 @@ public class shipMovement : MonoBehaviour
         {
             newpos.x = leftbound;
         }
-        rb.velocity = new Vector3(0, 5, 0);
+
         rb.MovePosition(newpos);
     }
 
