aea6e1252ea56380d69fc9cb71514ddb2b3b6978
Avery Crespi <averycrespi@gmail.com>
2019-02-15 22:40:14 -0800

    Make background move

---
+++ bcGameJam2019/Assets/ScrollScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(0, -5);
+        if (rb.position.y < -100) {
+            rb.MovePosition(Vector2.zero);
+        }
+    }
+}
