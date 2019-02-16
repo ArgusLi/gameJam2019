f4368cd7f02576f81002ba3a06ab34804e933c9d
Argus Li <argus@thembp.wifi.bcit.ca>
2019-02-15 22:44:23 -0800

    Added movement1

---
+++ bcGameJam2019/Assets/shipMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class shipMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveinput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveinput.normalized * speed;
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
