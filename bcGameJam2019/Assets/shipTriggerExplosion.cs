3e2451dab56772976cca34b4feedcd6220896ae8
Argus Li <argus@thembp.wifi.bcit.ca>
2019-02-16 07:09:55 -0800

    Collision Code in the works

---
+++ bcGameJam2019/Assets/shipTriggerExplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipTriggerExplosion : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("isTriggered", true);
+    }
+}
