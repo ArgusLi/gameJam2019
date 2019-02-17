0f555e025be75352dccfebc04eb3036e3dea8493
ArgusLi <contactme@chunkeili.com>
2019-02-17 12:57:05 -0800

    fixed cole's mistakes

--- bcGameJam2019/Assets/Scripts/God.cs
+++ bcGameJam2019/Assets/Scripts/God.cs
@@ -10,30 +10,36 @@ public class God : MonoBehaviour
     private float runningScoreTotal;
     private bool energyMode = false;
 
-    void launchNextFrame() {
+    void launchNextFrame()
+    {
         Debug.Log("Calling God.launchNextFrame");
         List<int[,]> boards = WorldGenerator.generateWorld(worlds, N);
-        for(int i = 0; i < worlds.Length; i++){
+        for (int i = 0; i < worlds.Length; i++)
+        {
             worlds[i].drawFrame(boards[i]);
         }
     }
-    
-    void Start() {
+
+    void Start()
+    {
         Debug.Log("Calling God.Start");
         worlds = gameObject.GetComponentsInChildren<World>();
-        for(int i = 0; i < worlds.Length; i++) {
-            worlds[i].transform.localPosition = new Vector3(100*i, 0, 0);
+        for (int i = 0; i < worlds.Length; i++)
+        {
+            worlds[i].transform.localPosition = new Vector3(100 * i, 0, 0);
         }
     }
-    
+
     //TODO: when ready, call launchNextFrame
 
-    public crash(float score, GameShip ship){
+    public void crash(float score, GameShip ship)
+    {
         runningScoreTotal += score;
         energyMode = true;
     }
 
-    IEnumerator LoadAfterDelay() {
+    IEnumerator LoadAfterDelay()
+    {
         yield return new WaitForSecondsRealtime(1);
         UnityEngine.SceneManagement.SceneManager.LoadScene("End");
     }
@@ -41,12 +47,16 @@ public class God : MonoBehaviour
     void OnDisable()
     {
         PlayerPrefs.SetFloat("score", runningScoreTotal);
-    void Update() {
-        for(int i = 0; i < worlds.Length; i++) {
-            if (!worlds[i].ReadyToDraw()) {
-                return;
+        void Update()
+        {
+            for (int i = 0; i < worlds.Length; i++)
+            {
+                if (!worlds[i].ReadyToDraw())
+                {
+                    return;
+                }
             }
-        }   
-        launchNextFrame();
+            launchNextFrame();
+        }
     }
 }
