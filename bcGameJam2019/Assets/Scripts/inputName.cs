262bfdb7dab3910d2f94838b9ac2a85855ff785e
ArgusLi <contactme@chunkeili.com>
2019-02-17 13:39:52 -0800

    input name complete

---
+++ bcGameJam2019/Assets/Scripts/inputName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class inputName : MonoBehaviour
+{
+    public TextMeshProUGUI tm;
+    private string playername;
+    // Start is called before the first frame update
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetString("playername", playername);
+    }
+}
