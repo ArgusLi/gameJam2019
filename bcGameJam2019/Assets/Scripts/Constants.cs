013bda094e92f62829a64f63c9482cc7badc6760
ArgusLi <contactme@chunkeili.com>
2019-02-17 09:14:03 -0800

    fixed title screen

---
+++ bcGameJam2019/Assets/Scripts/Constants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    private static int N;
+    private static float CamHeight;
+    private static float Unit;
+	public Constants()
+	{
+	}
+    public static void setN(int n)
+    {
+        N = n;
+    }
+    public static int getN()
+    {
+        return N;
+    }
+    public static void setCamHeight(float camHeight)
+    {
+        CamHeight = camHeight;
+    }
+    public static float getCamHeight()
+    {
+        return CamHeight;
+    }
+    public static void setUnit(float unit)
+    {
+        Unit = unit;
+    }
+    public static float getUnit()
+    {
+        return Unit;
+    }
+}
---
+++ bcGameJam2019/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr= GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.y += Time.deltaTime;
+
+        mat.mainTextureOffset = offset;
+    }
+}
